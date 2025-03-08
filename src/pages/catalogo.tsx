11eef1db0894798911f2423510254254ccdc1831
Mariana <marianagg0412@gmail.com>
2025-03-08 12:16:21 -0500

    feat(Catalogo): Added functionality

--- src/pages/catalogo.tsx
+++ src/pages/catalogo.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import ProductCard from '@/components/catalog/ProductCard';
 import { tw } from 'twind';
 import { useRouter } from 'next/router';
@@ -7,13 +7,12 @@ import CHeader from '@/components/atomic-design/organisms/CatalogHeader';
 import { fetchProducts } from '@/api/products';
 import { Product } from '@/interfaces/products';
 
-
 const Catalogo = () => {
   const router = useRouter();
   const [products, setProducts] = useState<Product[]>([]);
   const [isAdmin, setIsAdmin] = useState(false);
-  const [filters, setFilters] = useState({ category: '', use: '', specialty: '' });
-
+  type FilterType = 'category' | 'use' | 'specialty' | 'price';
+  const [filters, setFilters] = useState<{ [key in FilterType]: (string | number)[] }>({ category: [], use: [], specialty: [], price: [] });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -41,9 +40,9 @@ const Catalogo = () => {
 
   const filteredProducts = products.filter(product => {
     return (
-      (!filters.category || product.category === filters.category) &&
-      (!filters.use || product.use === filters.use) &&
-      (!filters.specialty || product.specialty === filters.specialty)
+      (filters.category.length === 0 || filters.category.includes(product.category)) &&
+      (filters.specialty.length === 0 || filters.specialty.includes(product.specialty)) &&
+      (filters.price.length === 0 || filters.price.includes(product.price))
     );
   });
 
@@ -51,12 +50,33 @@ const Catalogo = () => {
   const prices = Array.from(new Set(products.map(product => product.price)));
   const specialties = Array.from(new Set(products.map(product => product.specialty)));
 
-  
+  const handleFilterChange = (filterType: FilterType, value: string | number) => {
+    setFilters(prevFilters => {
+      const newFilterValues = prevFilters[filterType].includes(value)
+        ? prevFilters[filterType].filter((item: string | number) => item !== value)
+        : [...prevFilters[filterType], value];
+      return {
+        ...prevFilters,
+        [filterType]: newFilterValues
+      };
+    });
+  };
+
+  const handleClearFilters = () => {
+    setFilters({ category: [], use: [], specialty: [], price: [] });
+  };
 
   return (
     <div className={tw`container mx-auto `}>
-      <Header/>
-      <CHeader categories={categories} prices={prices} specialties={specialties} />
+      <Header />
+      <CHeader 
+        categories={categories} 
+        prices={prices} 
+        specialties={specialties} 
+        onFilterChange={handleFilterChange} 
+        filters={filters} 
+        onClearFilters={handleClearFilters}
+      />
       <div className={tw`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-10`}>
         {filteredProducts.length > 0 ? (
           filteredProducts.map((product) => <ProductCard key={product.id} product={product} />)
@@ -69,8 +89,3 @@ const Catalogo = () => {
 };
 
 export default Catalogo;
-
-
-
-
-
